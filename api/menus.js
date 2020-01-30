cc5897de957826210636c5609facd9bf67893ee4
Esteban Munch Jones <esteban.munch.jones@gmail.com>
2020-01-30 17:32:44 +0000

    till DELETE /api/menus/:menuId

---
+++ api/menus.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const menusRouter = express.Router( { mergeParams: true } );
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menusRouter.param('menuId', (req, res, next, menuId)=>{
+    db.get(`SELECT * FROM Menu WHERE id = ${menuId}`, (err, menu)=>{
+        if(err){
+            return next(err);
+        }
+        if(!menu){
+            return res.sendStatus(404);
+        }
+        req.menu = menu;
+        next();
+    })
+});
+
+menusRouter.get('/', (req, res, next)=>{
+    db.all(`SELECT * FROM Menu`, (err, menus)=>{
+        if(err){
+            return next(err);
+        }
+        res.status(200).json( { menus: menus} );
+    })
+});
+
+menusRouter.post('/', (req, res, next)=>{
+    const title = req.body.menu.title;
+    if(!title){
+        return res.sendStatus(400);
+    }
+    const sql = `INSERT INTO Menu (
+        title) VALUES (
+        $title)`;
+    const value = {
+        $title: title
+    };
+
+    db.run(sql, value, function(err){
+        if(err){
+            return next(err);
+        }
+        db.get(`SELECT * FROM Menu WHERE id = ${this.lastID}`, (err,menu)=>{
+            if(err){
+                return next(err);
+            }
+            res.status(201).json( { menu: menu} );
+        })
+    })
+});
+
+menusRouter.get('/:menuId', (req, res, next)=>{
+    res.status(200).json( { menu: req.menu } );
+});
+
+menusRouter.put('/:menuId', (req, res, next)=>{
+    const title = req.body.menu.title;
+    if(!title){
+        return res.sendStatus(400);
+    };
+    
+    const sql = `UPDATE Menu SET title = $title WHERE id = $id `;
+    const values = {
+        $title: title,
+        $id: req.params.menuId
+    };
+    db.run(sql, values, (err)=>{
+        if(err){
+            return next(err);
+        }
+        db.get(`SELECT * FROM Menu WHERE id = ${req.params.menuId}`, (err, menu)=>{
+            if(err){
+                return next(err);
+            }
+            res.status(200).json( { menu: menu } );
+        })
+    })
+});
+
+menusRouter.delete('/:menuId', (req, res, next)=>{
+    db.get(`SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId}`, (err, menuItem)=>{
+        if(err){
+            return next(err);
+        }
+        if(menuItem){
+            return res.sendStatus(400);
+        }
+        const sql = `DELETE FROM Menu WHERE id = $id`;
+        const value = {
+            $id: req.params.menuId
+        };
+        db.run(sql, value, (err)=>{
+            if(err){
+                return next(err);
+            }
+            res.sendStatus(204);
+        })
+    })
+});
+
+
+
+module.exports = menusRouter;
--- api/timesheets.js
+++ api/timesheets.js
@@ -4,6 +4,19 @@ const sqlite3 = require('sqlite3');
 const timesheetsRouter = express.Router( { mergeParams: true } );
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetsRouter.param('timesheetId', (req, res, next, timesheetId)=>{
+    db.get(`SELECT * FROM Timesheet WHERE id = ${timesheetId}`, (err, timesheet)=>{
+        if(err){
+            return next(err);
+        }
+        if(!timesheet){
+            return res.sendStatus(404);
+        }
+        req.timesheet = timesheet;
+        next();
+    })
+});
+
 timesheetsRouter.get('/',(req, res, next)=>{
     db.all(`SELECT * FROM Timesheet WHERE employee_id = ${req.params.employeeId}`, (err, timesheets)=>{
         if(err){
@@ -50,5 +63,54 @@ timesheetsRouter.post('/', (req, res, next)=>{
         })
     })
 });
+
+timesheetsRouter.put('/:timesheetId', (req, res, next)=>{
+    const hours = req.body.timesheet.hours;
+    const rate = req.body.timesheet.rate;
+    const date = req.body.timesheet.date;
+    if(!hours || !rate || !date){
+        return res.sendStatus(400);
+    }
+
+    const sql = `UPDATE Timesheet SET 
+        hours = $hours,
+        rate = $rate,
+        date = $date,
+        employee_id =  $employeeId
+        WHERE id = $id`;
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: req.params.employeeId,
+        $id: req.params.timesheetId
+    };
+
+    db.run(sql, values, (err)=>{
+        if(err){
+            return next(err);
+        }
+        db.get(`SELECT * FROM Timesheet WHERE id = ${req.params.timesheetId}`, (err, timesheet)=>{
+            if(err){
+                return next(err);
+            }
+            res.status(200).json( { timesheet: timesheet } );
+        })
+    })
+});
+
+timesheetsRouter.delete('/:timesheetId', (req, res, next)=>{
+    const sql = `DELETE FROM Timesheet WHERE id = $id`;
+    const value = {
+        $id: req.params.timesheetId
+    };
+    db.run(sql, value, (err)=>{
+        if(err){
+            return next(err);
+        }
+        res.sendStatus(204);
+    })
+});
+
 
 module.exports = timesheetsRouter;
